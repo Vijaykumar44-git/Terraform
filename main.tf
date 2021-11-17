75bcb342f34e87f7c1029f7220913766b5601a4a
Vijaykumar44-git <80778418+Vijaykumar44-git@users.noreply.github.com>
2021-11-17 10:56:58 +0530

    Added output attribute

--- main.tf
+++ main.tf
@@ -1,11 +1,17 @@
 provider "aws" {
-   region = "ap-south-1"
+  region = "ap-south-1"
 }
 
-resource "aws_vpc" "vpc1" {
-   cidr_block = "10.0.0.0/16"
-   tags = {
-      name = "VPC"
+variable "vpcname" {
+  type    = string
 }
+resource "aws_vpc" "test" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = var.vpcname
+  }
 }
-   
+
+output "vpcid" {
+   value = aws_vpc.test.id
+}
